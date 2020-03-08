31f23e21dd2bd4a07b0310469ffab6e03aadb604
Taugrim <q472180926@gmail.com>
2020-03-08 17:14:36 +0200

    senfile

--- work with filesystem/readFile.js
+++ work with filesystem/readFile.js
@@ -3,19 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function errr(err) {
-    if (err) {
-        console.error(err);
-    }
-}
-;
+
+var http = require('http');
 var fs = require('fs');
-fs.writeFile("file.tmp", "data", function (err) {
-    errr(err)
-    fs.rename("file.tmp", "new.tmp", function (err) {
-        errr(err)
-        fs.unlink("new.tmp"), function (err) {
-            errr(err)
-        }
+var url = require('url');
+var path = require('path');
+var ROOT = __dirname + "/public/";
+http.createServer(function (req, res) {
+    !chechAccess(req) == true ? nA(req, res) : null;
+    sendFileSafe(url.parse(req.url).pathname, res);
+}).listen(3000)
+
+function  sendFileSafe(filePath, res) {
+    try{
+        filePath=deccodeUruComponent(filePath)
+    }
+    catch (e){
+         setRes(res,400,"br")
+    }
+    
+     if (~filePath.indexOf('\0')){
+     setRes(res,404,"br")
+            return;
+    }
+    filePath=path.normalize(path.join(ROOT,filePath))
+    
+    fs.stat(filePath, function (err, stats) {
+    if (err || !stats.isFile()){
+      setRes(res,404,"nf")
+    } 
+sendFile(filePath, res);
+} )
+}
+function setRes(res,code,message) {
+   
+ res.statusCode = code;
+    res.end(message)
+    return;
+}
+
+function nA(req, res) {
+   setRes(res,403,"na")
+    return;
+}
+function sendFile(filePath, res) {
+    fs.readFile(filePath, function (err, content) {
+        if (err) throw err
+        var mime = require('mime').lookup(filePath);
+        res.setHeader('Content-Type', mime + "; charset=utf-8");
+        res.end(content);
     })
-});
+}
+
+
+
