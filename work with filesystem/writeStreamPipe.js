0a04aa6cc5e88b31c13ec6708a64efe968add9b4
Taugrim <q472180926@gmail.com>
2020-03-09 02:31:21 +0200

    PIPE prod

---
+++ work with filesystem/writeStreamPipe.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var http = require('http');
+var fs = require('fs');
+new http.Server(function (req, res) {
+    if(req.url ) {
+//    if(req.url == "/" + __felename ) {
+//        var file = new fs.FileReadStream("writeFile");
+        var file = new fs.FileReadStream(__filename);
+    sendFile(file,res);
+    } 
+}).listen(3000,'127.0.0.1');
+function sendFile(file,res) {
+   file.pipe(res);
+   file.pipe(process.stdout);
+   file.on('error',(err)=>{
+       res.statusCode=500;
+       res.end('server error');
+       console.error(err);
+   });
+   file.on('open',()=>{console.log('open');});
+   file.on('close',()=>{console.log('close');});
+   res.on('close',()=>{console.log('close');file.destroy();});
+}
+function event(obj,event,fun) {
+//    obj.on(event,fun());
+};
