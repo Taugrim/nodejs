c1611e7cb3b03e08f4e5ba62d02c2b586dfdd807
Taugrim <q472180926@gmail.com>
2020-03-12 03:39:49 +0200

    sart add abstract test

---
+++ seleniumjs/test/main.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var q=require('mocha')
+var chai=require('chai')
+const expect=chai.expect
+//console.log(q.test('qqq' ,function () {
+//    return false
+//}
+//        
+
+describe('qqqqq', function () {
+    it('',()=>{
+        var qq=fun1();
+    expect(fun1('q')).to.equal(sd()) })
+})
+
+function fun1(q) {
+    return q
+}
+var sd=()=>'q'
+var ex=(q1,q2)=> expect(q1).to.equal(q2)
+describe('www', function () {
+    it('',()=>{ex('q',sd())})})
+ex('q','w')
+
+
