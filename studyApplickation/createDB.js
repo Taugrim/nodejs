2c7360d72e3f809d166b28bd2da586115a9707b1
bart <bart@HTC-HERO5189>
2020-03-13 23:26:22 +0300

    sreated schena=class and objects into base

--- studyApplickation/createDB.js
+++ studyApplickation/createDB.js
@@ -1,23 +1,40 @@
-var MongoClient = require('mongodb').MongoClient
-  , format = require('util').format;
+var User=require('./models/user').User
+var user=new User({
+    username:"Test_name2",
+    password:"password"
+})
+user.save(function (err,user,affected) {
+    console.log(arguments)
+    User.findOne({username:"Test_name2"},function (err,user) {
+        console.log(arguments)
+    })
+})
 
-MongoClient.connect('mongodb://127.0.0.1:27017', function(err, db) {
-  if (err) throw err;
-var mdb=db.db('chat')
-  var collection = mdb.collection('test_insert');
-//  var collection =collection db.collection('test_insert');
-  collection.remove({}, function(err, affected) {
-    if (err) throw err;
 
-    collection.insert({a: 2}, function(err, docs) {
 
-      var cursor = collection.find({a: 2});
-      cursor.toArray(function(err, results) {
-        console.dir(results);
-        // Let's close the db
-        db.close();
-      });
-    });
 
-  });
-});
+
+////var MongoClient = require('mongodb').MongoClient
+//  , format = require('util').format;
+//
+//MongoClient.connect('mongodb://127.0.0.1:27017', function(err, db) {
+//  if (err) throw err;
+//var mdb=db.db('chat')
+//  var collection = mdb.collection('test_insert');
+////  var collection =collection db.collection('test_insert');
+//  collection.remove({}, function(err, affected) {
+//    if (err) throw err;
+//console.log(arguments)
+//console.log(arguments.length)
+//    collection.insert({a: 2}, function(err, docs) {
+//
+//      var cursor = collection.find({a: 2});
+//      cursor.toArray(function(err, results) {
+//        console.dir(results);
+//        // Let's close the db
+//        db.close();
+//      });
+//    });
+//
+//  });
+//});
