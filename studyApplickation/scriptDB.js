7222e0dac34c1fd2080b243850d0161ec5312f62
bart <bart@HTC-HERO5189>
2020-03-14 00:41:26 +0300

    refactoring2

--- studyApplickation/scriptDB.js
+++ studyApplickation/scriptDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('./libs/mongoose');
 var async = require('async')
-var User = require('./models/user').User
+
 
 async.series([
     open,
@@ -18,70 +18,18 @@ function dropDatabase(callback) {
     db.dropDatabase(callback)
 }
 function createUsers(callback) {
-    async.parallel([
-        function (callbac) {
-            var vasya = new User({username: "vasya", password: "123"})
-            vasya.save(function (err) {
-                callbac(err, vasya)
-            })
-        },
-        function (callbac) {
-            var vasya1 = new User({username: "vasya", password: "123"})
-            vasya1.save(function (err) {
-                callbac(err, vasya1)
-            })
-        },
-        function (callbac) {
-            var petya = new User({username: "petya", password: "456"})
-            petya.save(function (err) {
-                callbac(err, petya)
-            })
-        },
-        function (callbac) {
-            var admin = new User({username: "admin", password: "qwe"})
-            admin.save(function (err) {
-                callbac(err, admin)
-            })
-        }], callback
-            )
+    var User = require('./models/user').User
+    var users=[
+        {username: "vasya", password: "123"},
+        {username: "vasya", password: "123"},
+        {username: "petya", password: "456"},
+        {username: "admin", password: "qwe"}
+    ]
+    async.each(users,function (userData,callback) {
+      var user=  new User(userData)
+      user.save(callback)
+    },callback)
 }
 function close(callback) {
     mongoose.disconnect(callback)
 }
-//mongoose.connection.on('open', function () {
-//    var db = mongoose.connection.db;
-//    db.dropDatabase(function (err) {
-//        if (err)
-//            throw  err
-//        console.log("OK")
-//        async.parallel([
-//            function (callbac) {
-//                var vasya = new User({username: "vasya", password: "123"})
-//                vasya.save(function (err) {
-//                    callbac(err, vasya)
-//                })
-//            },
-//            function (callbac) {
-//                var vasya1 = new User({username: "vasya", password: "123"})
-//                vasya1.save(function (err) {
-//                    callbac(err, vasya1)
-//                })
-//            },
-//            function (callbac) {
-//                var petya = new User({username: "petya", password: "456"})
-//                petya.save(function (err) {
-//                    callbac(err, petya)
-//                })
-//            },
-//            function (callbac) {
-//                var admin = new User({username: "admin", password: "qwe"})
-//                admin.save(function (err) {
-//                    callbac(err, admin)
-//                })
-//            }],
-//                function (err, result) {
-//                    console.log(arguments)
-//                    mongoose.disconnect()
-//                })
-//    })
-//})
