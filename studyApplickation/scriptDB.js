020b5a17c6d9fc0574a0820f6971ca7c64e774fc
bart <bart@HTC-HERO5189>
2020-03-14 00:54:06 +0300

    refactoring3

--- studyApplickation/scriptDB.js
+++ studyApplickation/scriptDB.js
@@ -5,10 +5,12 @@ var async = require('async')
 async.series([
     open,
     dropDatabase,
-    createUsers,
-    close
+    reqModels,
+    createUsers
+   
 ],function (err,results) {
- console.log(arguments)   
+ console.log(arguments) 
+    mongoose.disconnect()
 })
 function open(callback) {
     mongoose.connection.on('open', callback)
@@ -17,8 +19,14 @@ function dropDatabase(callback) {
     var db = mongoose.connection.db;
     db.dropDatabase(callback)
 }
+function reqModels(callback) {
+     require('./models/user')
+     async.each(Object.keys(mongoose.models),function (modelName,callback) {
+                mongoose.models[modelName].ensureIndexes(callback)
+            },callback)
+}
 function createUsers(callback) {
-    var User = require('./models/user').User
+   
     var users=[
         {username: "vasya", password: "123"},
         {username: "vasya", password: "123"},
@@ -26,7 +34,7 @@ function createUsers(callback) {
         {username: "admin", password: "qwe"}
     ]
     async.each(users,function (userData,callback) {
-      var user=  new User(userData)
+      var user=  new mongoose.models.User(userData)
       user.save(callback)
     },callback)
 }
