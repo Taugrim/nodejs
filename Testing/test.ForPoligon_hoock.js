0340142da25f2025028ae1358ec390a6ba64998f
bart <bart@HTC-HERO5189>
2020-03-28 21:33:29 +0300

    add after /before

---
+++ Testing/test.ForPoligon_hoock.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+let poligon = require('./poligon');
+describe("suite hoocks", function() {
+
+beforeEach(() => console.log("beforeEach"))
+afterEach(() => console.log("afterEach")) 
+before(()=> console.log("before "))
+after(() => console.log("after "))
+it('empty test', function () {})
+it('test1', function () {})
+
+})
